bdce58070f5d119832e39fbfcce3adf37704e903
N3O <nothing@internet.comx>
2021-05-10 09:54:47 +1200

    exclude data

--- harness/dyke.harness.py
+++ harness/dyke.harness.py
@@ -3,3 +3,5 @@ import os
 print(os.getcwd())
 
 os.system("python " + os.getcwd() + "/experiments/dyke.py 100 100 0 10 0 100 0.01")
+os.system("python " + os.getcwd() + "/experiments/dyke.py 100 100 0 10 0 100 0.01")
+os.system("python " + os.getcwd() + "/experiments/dyke.py 50 100 0 10 0 50 0.01")
