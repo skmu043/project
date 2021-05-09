ca64616bec6ffc3f8d68695f1545d78185af6a43
N3O <nothing@internet.comx>
2021-05-09 16:43:38 +1200

    updates

--- init.py
+++ init.py
@@ -1,36 +1,30 @@
 import os
-structure_check = ['harness','experiments','data','analytics',]
+structure_ = ['harness','experiments','data','analytics',]
 
 def pre_flight_checks():
     check = True
-    for s_check in structure_check:
-        if(os.path.isdir(s_check)==False):
+    for si in structure_:
+        if(os.path.isdir(si)==False):
             check = False
-    #print(check)
 
 pre_flight_checks()
-
 #print(os.listdir(os.getcwdb()))
 
-
-
-
 while True:
-    print (30 * "-" , "MENU" , 30 * "-")
-    for s in structure_check:
-        print(structure_check.index(s),s)
+    for si in structure_:
+        print(structure_.index(si),si)
 
-    print (67 * "-")
-    choice = int(input("Enter your choice [0-"+ str(len(structure_check) - 1)+ "]: "))
+    select = int(input("Select [0-"+ str(len(structure_) - 1)+ "]: "))
 
-
-    if(choice <= len(structure_check)-1):
-        print(len(structure_check))
+    if(select <= len(structure_)-1):
+        #print(len(structure_))
         #print(os.listdir(structure_check[0]))
-        for s in os.listdir(structure_check[0]):
-            print(os.listdir(structure_check[0]).index(s),s)
-        choice = int(input("Enter your choice [0-"+ str(len(os.listdir(structure_check[0])) - 1)+ "]: "))
-        if(choice <= len(os.listdir(structure_check[0]))-1):
+        for si in os.listdir(structure_[select]):
+            print(os.listdir(structure_[select]).index(si),si)
+        run_select = int(input("Select [0-"+ str(len(os.listdir(structure_[select])) - 1)+ "]: "))
+        if(run_select <= len(os.listdir(structure_[select]))-1):
+            run_= structure_[select]
+            print(run_)
             os.system('python simulations/dyke.py')
         else:
             print("Invalid Selection")
