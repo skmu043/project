bab9409605c4e5ef26b9ebbb6f3b4db48dcbd9c0
N3O <nothing@internet.comx>
2021-05-09 21:22:27 +1200

    sync

--- harness/dyke.harness.py
+++ harness/dyke.harness.py
@@ -2,4 +2,4 @@ import os
 
 print(os.getcwd())
 
-os.system("python " + os.getcwd() + "/experiments/dyke.py 100 100 0 10 0 200 0.01")
+os.system("python " + os.getcwd() + "/experiments/dyke.py 100 100 0 10 0 100 0.01")
