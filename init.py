4d1abb3fec213d5f4866ac0e639d9b0181096ee0
N3O <nothing@internet.comx>
2021-05-09 16:36:13 +1200

    updates

--- init.py
+++ init.py
@@ -1,35 +1,39 @@
 import os
-structure_check = ['analytics','harness','results_data','simulations']
+structure_check = ['harness','experiments','data','analytics',]
 
 def pre_flight_checks():
     check = True
     for s_check in structure_check:
         if(os.path.isdir(s_check)==False):
             check = False
-    print(check)
+    #print(check)
 
 pre_flight_checks()
 
+#print(os.listdir(os.getcwdb()))
 
-print(os.listdir(os.getcwdb()))
 
-def print_menu(): 
+
+
+while True:
     print (30 * "-" , "MENU" , 30 * "-")
-   
     for s in structure_check:
         print(structure_check.index(s),s)
 
     print (67 * "-")
-  
+    choice = int(input("Enter your choice [0-"+ str(len(structure_check) - 1)+ "]: "))
 
-while True:          
-    print_menu()   
-    choice = int(input("Enter your choice [1-4]: "))
-    print(choice)
 
     if(choice <= len(structure_check)-1):
         print(len(structure_check))
-        print(os.listdir(structure_check[0]))
+        #print(os.listdir(structure_check[0]))
+        for s in os.listdir(structure_check[0]):
+            print(os.listdir(structure_check[0]).index(s),s)
+        choice = int(input("Enter your choice [0-"+ str(len(os.listdir(structure_check[0])) - 1)+ "]: "))
+        if(choice <= len(os.listdir(structure_check[0]))-1):
+            os.system('python simulations/dyke.py')
+        else:
+            print("Invalid Selection")
     else:
         print("Invalid Selection")
-        os.system('python simulations/dyke.py')
+
