5a18148083cca84f67d13cbc9bd462972e3207d3
N3O <nothing@internet.comx>
2021-05-11 17:54:42 +1200

    analytics working 1

--- harness/dyke.harness.py
+++ harness/dyke.harness.py
@@ -1,7 +1,6 @@
 import os
 
-#print(os.getcwd())
 
-#os.system("python " + os.getcwd() + "/experiments/dyke.py 100 100 0 10 0 100 0.01")
-#os.system("python " + os.getcwd() + "/experiments/dyke.py 100 100 0 10 0 100 0.01")
 os.system("python " + os.getcwd() + "/experiments/dyke.py 50 100 0 10 0 50 0.01")
+
+
