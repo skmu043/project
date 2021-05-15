5ad89bf43936d037ee4780f12998cd8ac6416009
N3O <nothing@internet.comx>
2021-05-15 16:42:23 +1200

    testing

--- harness/dyke.harness.py
+++ harness/dyke.harness.py
@@ -3,6 +3,6 @@ import os
 #"Args: K, R, P, E, start, end, step"
 #"e.g K=100, R=100, P=0, E=10, start=0, end=200, step=0.01"
 
-os.system("python " + os.getcwd() + "/experiments/dyke.py 100 100 0 10 0 200 0.01")
+os.system("python " + os.getcwd() + "/experiments/dyke_10.py 100 100 0 10 0 200 0.01")
 
 
