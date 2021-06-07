dc4bcb4c270239c89f9a3785b77ea49d9dc3fc44
N3O <nothing@internet.comx>
2021-06-07 18:28:14 +1200

    scipy roots biotic force go go

--- roots/scipy_roots_alpha.py
+++ roots/scipy_roots_alpha.py
@@ -112,11 +112,6 @@ for xi in np.arange(X1, Y1, 0.1):
 
 #TypeError: fsolve: there is a mismatch between the input and output shape of the 'func' argument 'f1'.Shape should be (2,) but it is (1,).
 
-
-def f2(x):
-    #return((x**3) + (2*(x**2)) - (2*x) - 5)
-    return(x**2 -1000)
-
 def plot_function():
     print("Plotting Sum  ... ")
     plt.figure(figsize=(20,10))
@@ -132,22 +127,16 @@ def plot_function():
     plt.show()
 
 print("Solving ...")
-sol = optimize.root(f1, [25], jac=False, method='hybr')
-
 true_zeros = []
 
 for _ in range(R):
     sol = optimize.root(f1, [_], jac=False, method='hybr')
-    #print(_,sol.x)
     if(sol.x >=0 and sol.x <= R):
         true_zeros.append(sol.x)
-
-
+print("All points ...")
+print(true_zeros)
+print("Unique Points ...")
 print(np.unique(np.array(true_zeros)))
-
-#sol = optimize.root(f1, [0], method='anderson')
-
-#print(sol.x)
 
 def plot_stable_points():
     plt.figure(figsize=(20,10))
@@ -156,18 +145,54 @@ def plot_stable_points():
     plt.ylabel('biotic force', fontsize=40)
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
-    #plt.ylim(X1, Y1)
-    #plt.xlim(0, end)
-    for x3 in true_zeros:
-        plt.axvline(x=x3)
-
+    for stable in true_zeros:
+        plt.axvline(x=stable)
     plt.axvline(x=0)
     plt.axhline(y=0)
-
-    plt.plot(x,y, 'r-',label = 'stable_points')
+    plt.plot(x,y, 'r-',label = 'temperature')
+    plt.legend(loc=5, prop={'size': 30})
     plt.show()
+
+
+print("Solving ...")
+true_zeros = []
+sign_change = ""
+
+if(y[0] < 0):
+    sign_change = "neg"
+if(y[0] > 0):
+    sign_change = "pos"
+
+if(y[0] == 0):
+    print("ZERO DETECTED")
+
+print(sign_change)
+
+for _ in range(R):
+    sol = optimize.root(f1, [_], method='df-sane')
+    if(sol.x >=0 and sol.x <= R):
+        true_zeros.append(sol.x)
+
+print(np.unique(np.array(true_zeros)))
+
+def plot_stable_points_sign_change():
+    plt.figure(figsize=(20,10))
+    plt.title('Stable Points Check', fontsize=40)
+    plt.xlabel('temperature', fontsize=40)
+    plt.ylabel('biotic force', fontsize=40)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    for stable in true_zeros:
+        plt.axvline(x=stable)
+    plt.axvline(x=0)
+    plt.axhline(y=0)
+    plt.plot(x,y, 'r-',label = 'temperature')
+    plt.legend(loc=5, prop={'size': 30})
+    plt.show()
+
 
 
 #plot_alphas()
 #plot_function()
 plot_stable_points()
+plot_stable_points_sign_change()
