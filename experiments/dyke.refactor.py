Fixed abundance needed initially and results during simulation being off by one 1999 - 2000 and fixed bad zero value

experiments/dyke.refactor.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-#mpl.use('macosx')
+
 
 if int(len(sys.argv)) != int(2):
     print("Args: shelve file name which contains all of > (K, R, P, E, start, end, step, EN, OE, LP_Z, RUN_ID)")
@@ -41,9 +41,7 @@ try:
 finally:
     s.close()
 
-
-SAMPLE_STEP = 50
-
+S_STEP = SAMPLE_STEP
 K = biotic_components_K
 R = essential_range_R
 P = external_perturbation_rate_P
@@ -77,10 +75,18 @@ local_population_index.sort()
 w = affects_w
 u = optimum_condition_u
 
+print(w)
+print(u)
+
 #Abundance values over time
 rAx = [[] for x in range(K)]
 #Abundance values over time scaled up by R (Essential Range)
 rAxR = [[] for x in range(K)]
+
+rNumberAlive = [[] for x in range(K)]
+
+#for _ in range(K):
+#    rNumberAlive[_].append(0)
 
 alpha = [[] for _ in range(K)]
 
@@ -98,10 +104,32 @@ for _ in range(K):
 
         alpha[_].append(new_alpha)
 
-        #print("alpha: ",alpha)
+        rAx[_].append(new_alpha)
+        rAxR[_].append(new_alpha * R)
+
+        if(new_alpha > 0):
+            rNumberAlive[_].append(1)
+        else:
+            rNumberAlive[_].append(0)
+
+print("alpha: ",alpha)
+print("Number Alive : ", rNumberAlive)
 
 rF = [[] for _ in range(N)]         #Biotic Force Values
 rE = [[] for _ in range(N)]         #A blank list for each Environment Variable
+
+initfSUM = [0 for _ in range(N)]
+for _ in range(K):
+    initfSUM[0] = initfSUM[0] + (alpha[_][-1] * w[0][_])
+
+rF[0].append(initfSUM[0])
+
+for _ in range(K):
+    if( _ in local_population_index):
+        initfSUM[1] = initfSUM[1] + (alpha[_][-1] * w[1][_])
+
+rF[1].append(initfSUM[1])
+
 
 for _ in range(N):
     rE[_].append(E[_])                 #Input the Start Temperatures
@@ -127,23 +155,12 @@ def update(step):
         for ei in range(N):
             #print(_)
             al.append(round((math.e) ** ((-1) * (((abs((E[ei])-u[ei][_])) ** 2) / (2*(OE[_]**2)))), ROUND))
-            #print("E: ", ei, " for E: ", E[ei]," with u = ", u[ei][_] , "and R = ", R,  " alpha is : ", al[-1])
-            #print("E",ei," -> ",E[ei],"- a=",al[-1])
-            #time scales - for each step - the next value is calculated (next abundance and next E (temperature))
-            #Now with timescales in mind, simply swithcing from the current value to the newly calculated value would indicate instantaneous change
-            #Instead instead of switching directly to the newly calculated value - we can approach that value via some function
-            #e.g Current E=5, new E=7, instead of using E=7 we will use a function where (E=5) approaches (E=7) so the final val may be E=6
-            # Keep timescales between 1 and 0 [1 = system is at the newly calculated value instantaneously whereas values closer to zero indicate slower timescales]
-            # Values outside 1 and 0 will cause errors as rates would go outside model bounds
 
-        # al = [0.5,0.5] (each species has its abundance calculated for E1 and E2 based on individual u values)
         new_alpha = 0
         if( _ in local_population_index):
-            new_alpha = np.prod(al) # If local population (product of abundances) (both local and global affect the local one)
+            new_alpha = np.prod(al)
         else:
-            new_alpha = al[0]       # Else take the first one as Eg
-
-        # For each species : If Species
+            new_alpha = al[0]
 
         k1 = new_alpha
         k2 = k1 + (k1 * step/2)
@@ -153,28 +170,19 @@ def update(step):
 
         alpha[_].append(yt)
 
-        #rAx[_].append(alpha[_][-1])
-        #rAxR[_].append(alpha[_][-1] * R)
         rAx[_].append(yt)
         rAxR[_].append(yt * R)
 
-    # ALPHA is the CHANGE to the previous Alpha !
+        if(yt > 0):
+            rNumberAlive[_].append(1)
+        else:
+            rNumberAlive[_].append(0)
 
-
-    # The above for loop has concluded ============ new code block below ============
-
-    # Issue Detected : multiplying all Fs from both - no differentiation given to sub population
-
-    # All K affect Eg - Fg
-    # All sub-pop K affect only El - Fl
-    # F [0 is Fg and 1 is Fl same as abundance above]
-
-    # w[0] affects EG whereas a subset of w[1] affects EL only
     for _ in range(K):
         fSUM[0] = fSUM[0] + (alpha[_][-1] * w[0][_])
 
-    F[0] = fSUM[0] #* 10                #FINALLY 10 has been removed !
-    newE = E[0] + (((0 + F[0]) * 1))    # [* 1] changes to [* step] -> if [* 10] is brought back above
+    F[0] = fSUM[0]
+    newE = E[0] + (((0 + F[0]) * 1))
     E[0] = E[0] + ((newE-E[0]) * temperature_time_scale)
     rF[0].append(F[0])
     rE[0].append(E[0])
@@ -185,8 +193,8 @@ def update(step):
         if( _ in local_population_index):
             fSUM[1] = fSUM[1] + (alpha[_][-1] * w[1][_])
 
-    F[1] = fSUM[1] #* 10                #FINALLY 10 has been removed !
-    newE = E[1] + (((0 + F[1]) * 1))    # [* 1] changes to [* step] -> if [* 10] is brought back above
+    F[1] = fSUM[1]
+    newE = E[1] + (((0 + F[1]) * 1))
     E[1] = E[1] + ((newE-E[1]) * temperature_time_scale)
     rF[1].append(F[1])
     rE[1].append(E[1])
@@ -209,14 +217,15 @@ if __name__ == '__main__':
     biotic_force_prime  =[]
     temperatures_prime  =[]
     simulation_run      =[]
+    rNumberAlive_prime  =[]
 
 
     # First Set of calculations have occurred during initilization so appending time 0
 
 
     # sampling
-    for Eg_temp in np.arange(1,100,SAMPLE_STEP):
-        for El_temp in np.arange(1,100,SAMPLE_STEP):
+    for Eg_temp in np.arange(1,100,S_STEP):
+        for El_temp in np.arange(1,100,S_STEP):
             print("Init : ", Eg_temp, El_temp)
             simulation_run.append((Eg_temp,El_temp))
             time.append(0)
@@ -225,27 +234,68 @@ if __name__ == '__main__':
             for xtime in np.arange (post_init_start, end, step):
                 update(step)
                 time.append(xtime)
-            #rAx.insert(0,0)
-            #rAxR.insert(0,0)
 
-            # Going forward - after each run is done
-            # Pack the data into separate data structures
-            # Zero out the in-use data structures for the run
-            # Re-initilize
-            # Run again
-            # e.g rE = [[1,2,3,4,5,6,7][2,4,6,7,8,9,0]] >>>> rE_prime.append(rE) >>>> rE = [[] for _ in N] (the initilization bit)
-            # rE_prime = [[[1,2,3,4,5,6,7][2,4,6,7,8,9,0]], [[1,2,3,4,5,6,7][2,4,6,7,8,9,0]], [[1,2,3,4,5,6,7][2,4,6,7,8,9,0]]]
-
+            #print("time: ", time)
+            print("Ltime", len(time))
             E_prime.append([Eg_temp, El_temp])
             F_prime.append(F)
+            for _ in range(K):
+                del alpha[_][-1]
             alpha_prime.append(alpha)
+            print("abundance : ",len(alpha[1]))
             rF_prime.append(rF)
+            print("rF: ",len(rF[0]))
+            print("rF: ", len(rF[1]))
             rE_prime.append(rE)
+            print("rE: ",len(rE[0]))
+            print("rE: ",len(rE[1]))
+            for _ in range(K):
+                 del rAx[_][-1]
             rAx_prime.append(rAx)
+            print("rAx: ", len(rAx[0]))
+            for _ in range(K):
+                del rAxR[_][-1]
             rAxR_prime.append(rAxR)
+            print("rAxR: ", len(rAxR[0]))
+            for _ in range(K):
+                del rNumberAlive[_][-1]
+            rNumberAlive_prime.append(rNumberAlive)
+            print("rNumberAlive: ",len(rNumberAlive[0]))
             time_prime.append(time)
             biotic_force_prime.append(biotic_force)
             temperatures_prime.append(temperatures)
+
+            #fig = plt.figure(figsize=(50,50))
+            #ax = fig.add_subplot(111, projection='3d', adjustable='box')
+            #ax.set_title(label = "EL/EG with Number Alive Single")
+            #ax.set_xlabel('X - EL', fontsize=10)
+            #ax.set_ylabel('Y - EG', fontsize=10)
+            #ax.set_zlabel('Z - Total Alive', fontsize=10)
+
+            #c_r = int(rE[1][-1])
+            #c_g = int(rE[0][-1])
+
+            #print(len(rE[0]))
+            #print(len(rE[1]))
+            #print(len(rNumberAlive))
+            #print(len(rNumberAlive[0]))
+            #sum_alives = []
+            #for x in range (len(rNumberAlive[0])):
+            #    current_sum = 0
+            #    for _ in range(K):
+            #        current_sum += rNumberAlive[_][x]
+            #    sum_alives.append(current_sum)
+
+            #print(len(sum_alives))
+            #if(c_r < 0 or c_g < 0 or c_r > 100 or c_g > 100 ):
+            #    ax.scatter(rE[1][0], rE[0][0], sum_alives[0],s=10, marker='.', color=(float(0), float(0), float(1)))
+            #    plt.plot(rE[1],rE[0], sum_alives , color=(float(0), float(0), float(1)))
+            #else:
+            #    plt.plot(rE[1],rE[0],sum_alives, color=(float(c_r/100), float(c_g/100), float(0.5)))
+            #    ax.scatter(rE[1][0], rE[0][0], 0, marker='.', s=10, color=(float(c_r/100), float(c_g/100), float(0.5)))
+            #    ax.scatter(rE[1][-1], rE[0][-1], sum_alives[-1], marker='*', s=15, color=(float(c_r/100), float(c_g/100), float(0.5)))
+
+            #plt.show()
 
             ###########################################################################################################################
             ###########################################################################################################################
@@ -254,6 +304,9 @@ if __name__ == '__main__':
             E = [Eg_temp,El_temp]
             F       = [0 for _ in range(N)]
             alpha = [[] for _ in range(K)]
+            rAx = [[] for x in range(K)]
+            rAxR = [[] for x in range(K)]
+            rNumberAlive = [[] for x in range(K)]
 
             # Fix applied - correctly generating alphas for global and local
             for _ in range(K):
@@ -268,16 +321,34 @@ if __name__ == '__main__':
                         new_alpha = al[0]       # Else take the first one as Eg
 
                     alpha[_].append(new_alpha)
-                    #print("alpha: ",alpha)
+                    rAx[_].append(new_alpha)
+                    rAxR[_].append(new_alpha * R)
+                    if(new_alpha > 0):
+                        rNumberAlive[_].append(1)
+                    else:
+                        rNumberAlive[_].append(0)
 
             rF = [[] for _ in range(N)]         #Biotic Force Values
             rE = [[] for _ in range(N)]         #A blank list for each Environment Variable
 
+            initfSUM = [0 for _ in range(N)]
+            for _ in range(K):
+                initfSUM[0] = initfSUM[0] + (alpha[_][-1] * w[0][_])
+
+            rF[0].append(initfSUM[0])
+
+            for _ in range(K):
+                if( _ in local_population_index):
+                    initfSUM[1] = initfSUM[1] + (alpha[_][-1] * w[1][_])
+
+            rF[1].append(initfSUM[1])
+
             for _ in range(N):
                 rE[_].append(E[_])                 #Input the Start Temperatures
 
-            rAx = [[] for x in range(K)]
-            rAxR = [[] for x in range(K)]
+
+
+
             time = []
             biotic_force = [[] for _ in range(K)]
             temperatures = []
@@ -300,6 +371,8 @@ def plot_alphas():
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
 
+    super_biotic_force = []
+
     for each_env_var in range(N):
         biotic_force = [[] for _ in range(K)]
         for y in range(K):
@@ -310,18 +383,21 @@ def plot_alphas():
         for _ in range(K):
             plt.plot(temperatures,biotic_force[_])
 
+
         plt.plot(temperatures,np.sum((np.array(biotic_force, dtype=float)), axis=0), lw=4)
-    plt.show()
+        super_biotic_force.append(np.sum((np.array(biotic_force, dtype=float)), axis=0))
 
-
-
-plot_alphas()
+    sum = []
+    #for _ in range(time_end/time_step):
+    #    sum.append(super_biotic_force[0][_] + super_biotic_force[1][_])
+    #plt.plot(temperatures, sum, lw=10)
+    #plt.show()
+#plot_alphas()
 
 
 def stable_points_space():
 
     stable_locations = []
-
     plt.figure(figsize=(30,30), dpi=200)
     plt.title('Regions', fontsize=40)
     plt.xlabel('EL', fontsize=40)
@@ -355,9 +431,11 @@ def stable_points_space():
 
 stable_points_space()
 
+mpl.use('macosx') #for the 3D magic
+
 def stable_points_space_3d_rotate():
 
-    fig = plt.figure(figsize=(50,50))
+    fig = plt.figure(figsize=(300,300))
     ax = fig.add_subplot(111, projection='3d', adjustable='box')
     ax.set_title(label = "EL/EG with Total Abundance")
     ax.set_xlabel('X - EL', fontsize=10)
@@ -373,8 +451,12 @@ def stable_points_space_3d_rotate():
         c_r = int(row[1][-1])
         c_g = int(row[0][-1])
 
+        print(len(row[1]))
+        print(len(row[0]))
+        print(len(rAx_prime[index_A]))
+
+
         row_abundance = []
-        row_abundance.append(0)
         decompose = rAx_prime[index_A]
         #print(decompose)
         run_length = len(decompose[0])
@@ -398,12 +480,65 @@ def stable_points_space_3d_rotate():
         else:
             #ax.scatter(row[1],row[0],row_abundance, color=(float(c_r/100), float(c_g/100), float(0.5)), s=1)
             plt.plot(row[1],row[0],row_abundance, color=(float(c_r/100), float(c_g/100), float(0.5)))
-            ax.scatter(row[1][0], row[0][0], 0, marker='.', s=10, color=(float(c_r/100), float(c_g/100), float(0.5)))
-            ax.scatter(row[1][-1], row[0][-1], row_abundance[-1], marker='*', s=15, color=(float(c_r/100), float(c_g/100), float(0.5)))
+            ax.scatter(row[1][0], row[0][0], row_abundance[0], marker='.', s=20, color=(float(c_r/100), float(c_g/100), float(0.5)))
+            ax.scatter(row[1][-1], row[0][-1], row_abundance[-1], marker='*', s=20, color=(float(c_r/100), float(c_g/100), float(0.5)))
 
-    index_A +=1
+        index_A +=1
 
     #plt.savefig("3d_abundance_" + str(RUN_ID)  + "-" +  str(random.randint(100, 999)) + ".png")
     plt.show()
 
 stable_points_space_3d_rotate()
+
+
+def stable_points_space_3d_rotate_number_alive():
+
+    fig = plt.figure(figsize=(300,300))
+    ax = fig.add_subplot(111, projection='3d', adjustable='box')
+    ax.set_title(label = "EL/EG with Number Alive")
+    ax.set_xlabel('X - EL', fontsize=10)
+    ax.set_ylabel('Y - EG', fontsize=10)
+    ax.set_zlabel('Z - Total Alive', fontsize=10)
+    ax.set_xlim([-10,100])
+    ax.set_ylim([-10,100])
+    #ax.set_zlim([0,50])
+
+    index_A = 0
+
+    #print(rNumberAlive_prime)
+
+    for row in rE_prime:
+        c_r = int(row[1][-1])
+        c_g = int(row[0][-1])
+
+        row_alive = []
+
+        decompose = rNumberAlive_prime[index_A]
+        run_length = len(decompose[0])
+
+        for x in range(run_length):
+            current_sum = 0
+            for item in decompose:
+                current_sum += item[x]
+            row_alive.append(current_sum)
+
+        #print(len(row_abundance))
+        #print(len(row[1]))
+        #print(len(row[1]))
+
+        if(c_r < 0 or c_g < 0 or c_r > 100 or c_g > 100 ):
+            ax.scatter(row[1][0], row[0][0], row_alive[0],s=10, marker='.', color=(float(0), float(0), float(1)))
+            #plt.plot(row[1][0], row[0][0], row_abundance[0], marker='x')
+            plt.plot(row[1],row[0], row_alive , color=(float(0), float(0), float(1)))
+        else:
+            #ax.scatter(row[1],row[0],row_abundance, color=(float(c_r/100), float(c_g/100), float(0.5)), s=1)
+            plt.plot(row[1],row[0],row_alive, color=(float(c_r/100), float(c_g/100), float(0.5)))
+            ax.scatter(row[1][0], row[0][0], row_alive[0], marker='.', s=20, color=(float(c_r/100), float(c_g/100), float(0.5)))
+            ax.scatter(row[1][-1], row[0][-1], row_alive[-1], marker='*', s=20, color=(float(c_r/100), float(c_g/100), float(0.5)))
+
+        index_A +=1
+
+    #plt.savefig("3d_alive_" + str(RUN_ID)  + "-" +  str(random.randint(100, 999)) + ".png")
+    plt.show()
+
+stable_points_space_3d_rotate_number_alive()

experiments/dyke_/dyke_og.py
@@ -8,7 +8,7 @@ R = 100        #Essential Range (defines where Biotic Components can be present)
 P = 0          #Perturbation
 OE = []        #Niche
 start = 0      #Time Start
-end = 500      #Time End
+end = 200      #Time End
 step= 0.1      #Time Step
 w = []         #Affects Parameter (ranges between -1 and 1 for each K)
 u = []         #Ideal Temperature for species (between 0 and R -> the essential range)
@@ -213,8 +213,8 @@ def plot_efp():
 for xtime in np.arange (start, end, step):
     update(step)
     time.append(xtime)
-    if(xtime == 100):
-        P += 20
+    #if(xtime == 100):
+    #    P += 20
 
 #    if(xtime == 70):
 #        P -= 10

harness/dyke.refactor.harness.py
@@ -1,11 +1,10 @@
 import random
 import os
 import shelve
-from multiprocessing import Process, Pool
 import time
+from multiprocessing import Process, Pool
 
 # Generating ALL Parameters
-
 SAMPLE_SIZE = int(1)
 SAMPLE_STEP = int(20)
 RUN_ID = int(time.time())
@@ -39,7 +38,7 @@ for ui in range(environment_components_N):
         print("Duplicate u's detected: Regenerating ...")
         optimum_condition_u[ui].clear()
         optimum_condition_u[ui] = [random.uniform(0, essential_range_R) for _ in range(biotic_components_K)]
-# End Generating Parameters
+
 
 # Create Shelve to store parameters being sent to experiment run
 exp_name = "dyke.refactor"
@@ -86,10 +85,10 @@ def print_time():
     print(current_time)
 
 
-def run_it(shelve_file):
-    print("Running with PHI : ", shelve_file)
-    os.system("python3.9 " + os.getcwd() + "/experiments/" + exp_name + ".py " + str(shelve_file))
-    print(shelve_file)
+def run_it(shelve_file_):
+    print("Running with Shelve Input : ", shelve_file_)
+    os.system("python3.9 " + os.getcwd() + "/experiments/" + exp_name + ".py " + str(shelve_file_))
+    print(shelve_file_)
 
 
 if __name__ == '__main__':
@@ -103,12 +102,12 @@ if __name__ == '__main__':
     #     for Eg_temp in np.arange(1,100,SAMPLE_STEP):
     #         for El_temp in np.arange(1,100,SAMPLE_STEP):
     #             print("Init : ", Eg_temp, El_temp)
-    #             > Open Shelve and update and Close Shelve note run_it is its own run so open run_it's local shelve and update that
+    #             > Open Shelve and update and Close Shelve note run_it
+    #             is its own run so open run_it's local shelve and update that
     # which creates the problem of run_it using different w and u values
     # so solution could be for this one shelve, generate the start pairs and then iterate ...
-
-    # start_trajectories = ((1,1, shelve_file), (5,5, shelve_file), (10,10, shelve_file), (15.15, shelve_file), (20,20, shelve_file))
-    #pool.map (run_it, start_trajectories)
-
+    # start_trajectories = ((1,1, shelve_file), (5,5, shelve_file),
+    # (10,10, shelve_file), (15.15, shelve_file), (20,20, shelve_file))
+    # pool.map (run_it, start_trajectories)
     # run_it(start_traj)
     # 